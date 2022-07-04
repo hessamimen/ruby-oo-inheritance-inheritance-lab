662a6836c8ab7f9fecd7c4aac0713b9e18c2dd99
Hessam Imen <hessam.imen@gmail.com>
2022-07-04 17:54:26 +1000

    Done with assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
 
+    def initialize()
+        @knowledge = []
+    end
+
+    def learn(str)
+        @knowledge.push(str)
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
